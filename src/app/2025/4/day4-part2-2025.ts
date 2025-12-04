02ea192d5d9eea7941294dadeb062b269f5ce6b9
Léo Ladevie <leo.ladevie-exterieur@fiducial.net>
2025-12-04 08:11:03 +0100

    feat: Day 4 part 2

--- src/app/2025/4/day4-part2-2025.ts
+++ src/app/2025/4/day4-part2-2025.ts
@@ -1,13 +1,27 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Solution} from "../../puzzle-day/solution";
+import {GridP2} from "./gridP2";
 
 @Injectable()
-export class Day4Part22025 extends Solution<string, string> {
+export class Day4Part22025 extends Solution<string, number> {
 
-    problemName = 'Printing Department - Part 2';
+    problemName = 'Printing Department - Part 1';
 
-    protected override process(input: string): string {
-        return 'Method not implemented.';
+    protected override process(input: string): number {
+        let acc: number = 0;
+        let newRollsRemovedNb = -1;
+        const grid = new GridP2(this.inputUtils.toString2DArray(input));
+        console.log(grid.toString());
+        while (newRollsRemovedNb !== 0) {
+            const newRollsRemoved = grid.findRollsAccessedByForkLift();
+            newRollsRemovedNb = newRollsRemoved.length;
+            if (newRollsRemovedNb) {
+                console.log(newRollsRemoved);
+                grid.replaceRollsByNothing(newRollsRemoved);
+                console.log(grid.toString());
+            }
+            acc += newRollsRemovedNb;
+        }
+        return acc;
     }
-
 }
