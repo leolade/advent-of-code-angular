4232c02aaf8fbe7d7c0feb7629f2197ade638f4f
Léo Ladevie <leo.ladevie-exterieur@fiducial.net>
2025-12-05 09:28:57 +0100

    feat: Day 5 part 1

---
+++ src/app/2025/5/day5-part2-2025.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import {Solution} from "../../puzzle-day/solution";
+
+@Injectable()
+export class Day5Part22025 extends Solution<string, string> {
+
+    problemName = 'Cafeteria - Part 2';
+
+    protected override process(input: string): string {
+        return 'Method not implemented.';
+    }
+
+}
